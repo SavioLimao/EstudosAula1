e03c73a67bfad0bf8275bf94ceaa6e374aef5554
Sávio Limão <saviolimao@hotmail.com>
2024-07-11 14:39:13 -0300

    Codigo quebrado
    Codigo quebrou quando tentei envolopar elementos dentro de um componente que muda o CSS

--- src/ExpenseItem/ExpenseItem.jsx
+++ src/ExpenseItem/ExpenseItem.jsx
@@ -7,7 +7,7 @@ function ExpenseItem(props) {
       <div className="MainDiv">
         <ExpenseDate date={props.date} />
         <h2 className="Product">{props.title}</h2>
-        <div className="Price">{props.amount}</div>
+        <div className="Price">${props.amount}</div>
       </div>
     </CardBG>
   );
